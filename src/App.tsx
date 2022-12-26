da7fa3fda87b7fad851e5f01a1d06f5612e7dd5f
Sean O'Connor <so@seano.dev>
2022-12-26 16:18:55 -0700

    Update `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Fragment } from "react";
+import styled from "styled-components";
 import GlobalStyles from "./globalStyles";
+
+const _Main = styled.main`
+  min-height: inherit;
+  height: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
     <Fragment>
       <GlobalStyles />
+      <_Main></_Main>
     </Fragment>
   );
 }
